e97c7f2133f9afaf39d9e4c55e672b0cec53e882
Reika531 <r.miyazono531@gmail.com>
2024-05-14 07:51:32 +0900

    ファイルをコミット

---
+++ foreach.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $produce_data = ['name' => '玉ねぎ', 'price' => 200, 'area' => '北海道'];
+ 
+         //連想配列$prpduce_dataのキーと値を1つずつ順番に出力する
+         foreach ($produce_data as $key => $value) {
+             echo "{$key}:{$value}<br>";
+         }
+         ?>
+     </p>
+ </body>
+ 
+ </html>
